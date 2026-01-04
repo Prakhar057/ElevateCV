6dd0670d5dd111d2f9e90c67161a60485d510eda
Prakhar Gupta <guptaprakhar057@gmail.com>
2026-01-05 02:19:52 +0530

    made the resume preview page

--- client/src/Pages/Preview.jsx
+++ client/src/Pages/Preview.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import Loader from "../components/Loader";
+import ResumePreview from "../components/ResumePreview";
+import { ArrowLeftIcon } from "lucide-react";
+import { useEffect } from "react";
 
-const Preview = () => {
-  return (
-    <div>
-      <h1>Preview Page</h1>
+const Preview = ({ data }) => {
+  const { resumeId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [resumeData, setResumeData] = useState(null);
+
+  const loadResume = () => {
+    setResumeData(
+      dummyResumeData.find((resume) => resume._id === resumeId || null)
+    );
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadResume();
+  }, []);
+
+  return resumeData ? (
+    <div className="bg-slate-100">
+      <div className="max-w-3xl mx-auto py-10">
+        <ResumePreview
+          classes="py-4 bg-white"
+          data={resumeData}
+          template={resumeData.template}
+          accentColor={resumeData.accent_color}
+        />
+      </div>
     </div>
-  )
-}
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <p className="text-center text-6xl text-slate-400 font-medium">
+            Resume Not Found
+          </p>
+          <a
+            href="/"
+            className="mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors"
+          >
+            <ArrowLeftIcon className="mr-2 size-4" />
+            go to home
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Preview
+export default Preview;
